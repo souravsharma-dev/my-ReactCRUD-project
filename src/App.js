73bbc3058fb78034001886f48d327d02eee8b498
souravsharma-dev <sourav.sourav.sharma07@gmail.com>
2026-04-22 22:42:32 +0530

    change in UI

--- src/App.js
+++ src/App.js
@@ -95,7 +95,7 @@ const [form, setForm] = useState({
               style={{
                 background: 'orange',
                 color: 'white',
-                borderRadius: '5px',
+                borderRadius: '9px',
                 marginRight: '8px'
               }}><i class="ri-add-line"></i></button> 
               <button 
