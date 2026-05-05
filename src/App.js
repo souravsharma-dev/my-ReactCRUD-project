d801b63dceb86b7c3de93419c3bed24ad81115c8
souravsharma-dev <sourav.sourav.sharma07@gmail.com>
2026-05-05 20:34:05 +0530

    change in Content

--- src/App.js
+++ src/App.js
@@ -83,7 +83,7 @@ const [form, setForm] = useState({
       }}>
       <h1 style={{
         textAlign: 'center',
-        padding: '16px',
+        padding: '18px',
       }}>CRUD App</h1>
 
       <table className="table">
